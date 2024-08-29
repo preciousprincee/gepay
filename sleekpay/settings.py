48faf69c2e340c4aae0a345d505220c94144ed7b
Precious Ogbonnaya <pchukwuebuka964@gmail.com>
2024-08-29 10:25:04 +0100

    Update settings.py

--- sleekpay/settings.py
+++ sleekpay/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = 'django-insecure-^6n3k!k(vj%r#7=ob5nan=x81sj#z!41u1yq4zyuv#kd88(^^w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'now.sh', 'localhost']
 
 
 # Application definition
